708a42d7371fa2886a7b02bf2434d5c75981d61e
clfeng <18813292005@163.com>
2017-09-12 17:14:03 +0800

    add more code

--- jsCompatibleFn/jsFn.js
+++ jsCompatibleFn/jsFn.js
@@ -192,3 +192,324 @@ function setInnerText(element, text){
     element.innerText = text;
   }
 }
+// 获取计算后的样式
+function getComputedStyle(elem, attr){
+  if (window.getComputedStyle) {
+    return window.getComputedStyle(elem, null)[attr];
+  }else{
+    return document.style.currentStyle;
+  }
+}
+// 通过<link><style>元素取得CSSStyleSheet对象
+function getStyleSheet(elem){
+ return elem.sheet || elem.styleSheet;
+}
+
+// 获取元素在这个页面中偏移量的方法
+function getElementLeft(element){
+  var actualLeft = element.offsetLeft;
+  var current = element.offsetParent;
+  while(current != null){
+    actualLeft +=current.offsetLeft;
+    current = current.offsetParent;
+  }
+  return actualLeft;
+}
+function getElementTop(element){
+  var actualTop = element.offsetTop;
+  var current = element.offsetParent;
+  while(current != null){
+    actualTop +=current.offsetTop;
+    current = current.offsetParent;
+  }
+  return actualTop;
+}
+
+// 获取页面视口大小    
+function getViewport(){
+ if (document.compatMode == 'BackCompat') {
+  return {
+    width: document.body.clientWidth,
+    height: document.body.clientHeight
+  }
+}else{
+  return {
+    width: document.documentElment.clientWidth,
+    height: document.documentElemnt.clientHeight
+  }
+}
+}
+
+
+// 确定文档高度
+// 怪异模式时用document.body
+function getDocWH(){
+  if(document.compatMode == "backCompat"){
+   return {
+    docHeight: Math.max(document.body.scrollHeight, document.body.clientHeight),
+    docWidht: Math.max(document.body.scrollWidth, document.body.clientWidth)
+  }
+}else{
+    // document.compatMode == "CSS1Compat"
+    return {
+      docHeight: Math.max(document.documentElement.scrollHeight,document.documentElement.clientHeight),
+      docWidth: Math.max(document.documentElement.scrollWidth,document.documentElement.clientWidth)
+    }
+  }
+}
+
+// getBoundingClientRect()方法返回一个对象,对象具有left,right,top,bottom四个属性
+// 由于IE8-认为文档的左上角坐标是（2,2），其他浏览器则为(0,0)
+
+ //在严格模式下不可用，由于用到了arguments.callee
+ function getBoundingClientRect(element){
+  var scrollTop = document.body.scrollTop;
+  var scrollLeft = document.body.scrollLeft;
+  if (element.getBoundingClientRect) {
+    if (typeof arguments.callee.offset != "number") {
+      var temp = document.createElement('div');
+      temp.style.cssText = "position:absolute;top:0;left:0;";
+      document.body.appendChild(temp);
+      // offset用于纠正IE8-中的不一致
+      // - scrollTop处理的是页面被滚动的情况
+      arguments.callee.offset = -temp.getBoundingClientRect().top - scrollTop;
+      document.body.removeChild(temp);
+      temp = null;
+    }
+    var rect = element.getBoundingClientRect();
+    var offset = arguments.callee.offset;
+    return {
+      left: rect.left + offset,
+      right: rect.right + offset,
+      top: rect.top + offset,
+      bottom: rect.bottom + offset
+    }
+  }else{
+    var actualLeft = getElementLeft(element);
+    var actualTop = getElementTop(element);
+    return {
+      left:actualLeft - scrollLeft,
+      right: actualLeft -scrollLeft + element.offsetWidth,
+      top: actualTop -scrollTop,
+      bottom: actualTop - scrollTop + element.offsetHeight
+    }
+  }
+}
+
+(function (){
+  function draw(timestamp){
+    // 计算两次重回的时间间隔
+    var drawStart = (timeStamp || Date.now()),
+    diff = drawStart - startTime;
+    // 使用diff确定下一步的绘制时间
+    // 把startTime重写为这一次的绘制时间
+    startTime = drawStart;
+    // 重绘UI
+    requestAnimationFrame(draw);
+  }
+  var requestAnimationFrame = window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame||
+  window.webkitRequestAnimationFrame||
+  window.msRequestAnimationFrame,
+  statTime = window.mozAnimationStartTime || Date.now();
+  requestAnimationFrame(draw);
+})()
+
+// 获取地理位置绘制地图
+
+navigator.geolocation.getCurrentPosition(function (position){
+  drawMapCenteredAt(position.coords.latitude, position.coords.longitude);
+},function (error){
+  console.log("Error code "+ error.code);
+  console.log("Error message" + error.message);
+},{
+  enableHighAccuracy: true,
+  timeout: 500,
+  maximumAge: 25000
+})
+
+var watchId = navigator.geolocation.watchPosition(function (position){
+  drawMapCenteredAt(position.coords.latitude, position.coords.longitude);
+},function (error){
+  console.log("Error code "+ error.code);
+  console.log("Error message" + error.message);
+})
+clearWatch(watchId)
+
+// 文件API
+var fileList = document.getElementById("files-list");
+EventUtil.addHandler(fileList, "change",function (event){
+  var info = "",
+  output = document.getElementById("output"),
+  progress = document.getElementById("progress"),
+  files = EventUtil.getTarget(event).files,
+  type = "default",
+  reader = new FileReader();
+
+  if(/image/.test(files[0].type)){
+   reader.readAsDataURL(files[0]);
+   type = "image";
+ }else{
+   reader.readAsText(files[0]);
+ }
+ reader.onerror = function (){
+   output.innerHTML = "Could not read file, error code is "+
+   reader.error.code;
+ }
+ reader.onprogress = function (event){
+   if (event.lengthComputable) {
+     output.innerHTML = event.loaded + "/"+ event.total;
+   }
+ }
+ reader.onload = function (){
+   var html = "";
+   switch(type){
+     case "image":
+     html = "<img src=\""+reader.result + "\">";
+     break;
+     case "text":
+     html = reader.restul;
+     break;
+   }
+   output.innerHTML = html;
+ }
+})
+
+// 读取部分文件内容
+
+
+function blobSlice(blob, startByte, length){
+  if (blob.slice) {
+    return blob.slice(startByte, length);
+  }else if(blob.webkitSlice){
+    return blob.webkitSlice(startByte, length);
+  }else if (blob.mozSlice) {
+    return blob.mozSlice(startByte, length);
+  }else {
+    return null;
+  }
+}
+var fileList = document.getElementById("files-list");
+
+
+EventUtil.addHandler(fileList, "change",function (event){
+  var info = "",
+  output = document.getElementById("output"),
+  progress = document.getElementById("progress"),
+  files = EventUtil.getTarget(event).files,
+  type = "default",
+  reader = new FileReader(),
+  blob = blobSlice(files[0], 0, 32)
+
+  if (blob) {
+    reader.readAsText(blob);
+    reader.onerror = function (){
+      ouput.innerHTML = "Could not read file, error code is"+
+      reader.error.code;
+    };
+    reader.onload = function (){
+      output.innerHTML = reader.result;
+    }
+  }else{
+    alert("Your browser doesnt support slice()");
+  }
+})
+// 对象URL
+ 
+function createObjectURL(blob){
+  if (window.URL) {
+     return window.URL.createObjectURL(blob);
+  }else if (window.webkitURL) {
+     return window.webkitURL.createObjectURL(blob);
+  }else{
+     return null;
+  }
+}
+ 
+var fileList = document.getElementById("files-list");
+EventUtil.addHandler(fileList, "change",function (event){
+  var info = "",
+       output = document.getElementById("output"),
+       progress = document.getElementById("progress"),
+       files = EventUtil.getTarget(event).files,
+       type = "default",
+       reader = new FileReader(),
+       url = createObjectURL(files[0]);
+  if (url) {
+     if (/image/.test(files[0].type)) {
+       output.innerHTML = "<img src\"" + url + "\">";
+     }else{
+       output.innerHTML = "Not an image.";
+     }
+  }else{
+     output.innerHTML = "Your browser dosent support object URLs";
+  }
+})
+ function revokeObjectURL(url){
+  if (window.URL) {
+     window.URL.revokeObjectURL(url);
+  }else if (window.webkitURL) {
+     window.webkitURL.revokeObjectURL(url);
+  }
+}
+
+// 读取拖放文件
+var dropstart = document.getElementById("droptarget");
+function handleEvent(event){
+  var info = "",
+       output = document.getElementById("output"),
+       files, i, len;
+  Event.preventDefault(event);
+  if (event.type == "drop") {
+     files = event.dataTransfer.files;
+     i = 0;
+     len = files.length;
+ 
+     while(i<len){
+       info += files[i].name + "(" + files[i].type + "," + files[i].size + "bytes)<br>";
+          i++;
+     }
+     output.innerHTML = info;
+  }
+}
+ 
+// 必须取消dragenter dragover 和drop的默认行为
+EventUtil.addHandler(droptarget, "dragenter", handleEvent);
+EventUtil.addHandler(droptarget, "dragover", handleEvent);
+EventUtil.addHandler(droptarget, "drop", handleEvent);
+
+// 使用XHR上传文件
+var dropstart = document.getElementById("droptarget");
+function handleEvent(event){
+  var info = "",
+       output = document.getElementById("output"),
+       data, xhr,
+       files, i, len;
+  Event.preventDefault(event);
+  if (event.type == "drop") {
+     data = new FormData();
+     files = event.dataTransfer.files;
+     i = 0;
+     len = files.length;
+ 
+     while(i<len){
+          data.append("file"+i, files[i]);
+          i++;
+     }
+     xhr = new XMLHttpRequest();
+     xhr.open("post", "FileAPIExample.php", true);
+     xhr.onreadystatechange = function (){
+       if (xhr.readystate == 4) {
+          console.log(xhr.responseText)
+       }
+     }
+     xhr.send(data);
+  }
+}
+ 
+// 必须取消dragenter dragover 和drop的默认行为
+EventUtil.addHandler(droptarget, "dragenter", handleEvent);
+EventUtil.addHandler(droptarget, "dragover", handleEvent);
+EventUtil.addHandler(droptarget, "drop", handleEvent);
+ 
+ 
