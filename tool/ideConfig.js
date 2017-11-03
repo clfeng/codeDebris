6965ceaf17e67cae0c4a0ee8098c26cf103b6f11
clfeng <18813292005@163.com>
2017-11-03 17:07:36 +0800

    add the tool file

---
+++ tool/ideConfig.js
@@ -0,0 +1,59 @@
+/*
+vscode
+
+{
+    "editor.codeLens": true,
+    "editor.fontSize": 17,
+    "editor.tabSize": 2,
+    "editor.formatOnSave": false,
+    "files.associations": {
+    "*.vue": "vue"
+    },
+    "eslint.validate": [
+    "javascript",
+    "javascriptreact",
+    "html",
+    "vue",
+    {
+    "language": "html",
+    "autoFix": true
+    }
+    ],
+    "emmet.syntaxProfiles": {
+    "vue-html": "html",
+    "vue": [
+    "css",
+    "html",
+    "less"
+    ]
+    },
+    "editor.fontFamily": "Source Code Pro, 'Courier New', monospace",
+    "files.autoSave": "off",
+    "editor.insertSpaces": true,
+    "vetur.format.defaultFormatter.js": "vscode-typescript",
+    "javascript.format.insertSpaceBeforeFunctionParenthesis": true,
+    "window.zoomLevel": -1,
+    "window.menuBarVisibility": "default",
+    "workbench.statusBar.visible": true,
+    "workbench.activityBar.visible": true,
+    "workbench.colorTheme": "Monokai",
+    "sublimeTextKeymap.promptV3Features": true,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.snippetSuggestions": "top",
+    "editor.formatOnPaste": true
+}
+
+插件：
+
+sublime主题：Monokai
+vue使用插件：Vetur
+
+*/
+
+/*
+webstorm:
+小地图：codeGlance
+sublime主题： https://github.com/DmitryLanda/phpstorm-monokai
+下载之后将monokia.jar包引入
+
+*/
